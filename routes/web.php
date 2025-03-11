8eeed07aa3e9db0ee6ce0b6d80d0af61b162b77e
user638726 <lienjackson35@gmail.com>
2025-03-11 15:56:22 +0800

    建構網站Part2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+<?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 
-use App\Models\Student;// });
-//Route::get('/students_create', [StudentController::class, 'create']);
-//Route::get('/students_index', [StudentController::class, 'index']);
-//Route::get('/students_excel', [StudentController::class, 'excel']);
-//Route::get('/students_hello', [StudentController::class, 'sayHello']);
+Route::get('/', function () {
+    // return redirect()->route('students.index');
+    return redirect()->route('teachers.index');
+});
+
+// students
+Route::get('/students_excel', [StudentController::class, 'excel']);
+Route::get('/students_hello', [StudentController::class, 'sayHello']);
 Route::resource('students', StudentController::class);
+
+// teachers
+Route::resource('teachers', TeacherController::class);
